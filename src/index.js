8121855b58a140df12f8b66dc0b4749ed229319f
Yamauchi <yamauchi@neuron.eei.eng.osaka-u.ac.jp>
2020-03-29 23:11:24 +0900

    uikit-themeの位置を変更

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import Icons from 'uikit/dist/js/uikit-icons';
 
 import $ from 'jquery';
 
+
+import "./uikit-theme/custom.less";
+
 import { facilitiesMain } from "./facilitiesMain";
 import { concerthallMain } from "./concerthallMain";
 
-import "../node_modules/uikit/custom/custom.less";
 import "./css/main.css";
 
 $(function () {
